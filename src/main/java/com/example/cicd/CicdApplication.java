30b92e6b79c68eeca2436f071d6d60e09554d144
Your Name <you@example.com>
2024-03-02 17:14:44 +0530

    added src and pom.xml

---
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -0,0 +1,32 @@
+package com.example.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+	@GetMapping
+	ResponseEntity<?> welcome(){
+		return new ResponseEntity<>("Welcome to CICD",HttpStatus.OK);
+	}
+
+}
+
+
+echo "# cicd" >> README.md
+		git init
+		git add README.md
+		git commit -m "first commit"
+		git branch -M main
+		git remote add origin https://github.com/xploretech001/cicd.git
+		git push -u origin main
